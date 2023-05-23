9c9655627fb0c5670b46fd25b02abd09d8708176
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-23 09:58:04 +0900

    ts

--- todo-with-ts/src/components/Input.tsx
+++ todo-with-ts/src/components/Input.tsx
@@ -4,11 +4,27 @@ type myType = {
   color: string;
 }
 
-interface myInterface {
-  color: string;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement>  {
+  color?: string;
 }
 
-export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
+// interface Todo {
+//   tite: string;
+//   description: string;
+// }
+// 
+// type OptionalTodo = Partial<Todo>;
+// 
+// interface Props {
+//   a?: number;
+//   b?: string;
+// }
+//  
+// const obj: Props = { a: 5 };
+// // makes required 
+// const obj2: Required<Props> = { a: 5 };
+
+export default function Input(props: InputProps) {
   return (
     <input {...props} />
   );
