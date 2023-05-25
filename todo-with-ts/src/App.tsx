84ec2f961545ab36c5e7b258f57ad5589056bc64
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-25 10:50:41 +0900

    feat creating user input

--- todo-with-ts/src/App.tsx
+++ todo-with-ts/src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Input from './components/Input';
 import TodoList from './TodoList';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useEffect, useState } from 'react';
 import { Todo } from './types/todo';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -22,19 +22,33 @@ function App() {
   }, [todos]);
 
   const handleCreate = () => {
+    if (!todoName) 
+      return;
+
     const todo: Todo = {
       id: uuidv4(),
-      name: 'test',
+      name: todoName,
     }
+
     setTodos((prevState) => {
       return [...prevState, todo];
     });
+    setTodoName('');
   }
 
+  const handleInputChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+    setTodoName(value);
+  }
+  
+  const handleEnterPress = ({ key }: KeyboardEvent<HTMLInputElement>) => {
+    if (key !== 'Enter') 
+      return;
+    handleCreate();
+  }
   
   return (
     <div className="App">
-      <Input value={todoName} placeholder='todo name...'/>
+      <Input onKeyUp={handleEnterPress} onChange={handleInputChange} value={todoName} placeholder='todo name...'/>
       <button onClick={handleCreate}>create</button>
       <TodoList deleteTodo={deleteTodo} searchValue={searchValue} setSelectedTodo={setSelectedTodo} todos={todos}/>
     </div>
