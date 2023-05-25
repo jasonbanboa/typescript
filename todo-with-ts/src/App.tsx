5b7960c3b334f4638e39e65b9ebf70df1e2d0ab5
jasonbanboa <jasonbanboa123@gmail.com>
2023-05-25 09:32:22 +0900

    feat creating todo function

--- todo-with-ts/src/App.tsx
+++ todo-with-ts/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import Input from './components/Input';
 import TodoList from './TodoList';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Todo } from './types/todo';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [searchValue, setSearchValue] = useState<string>('');
   const [selectedTodo, setSelectedTodo] = useState<Todo['id'][]>([]);
+  const [todoName, setTodoName] = useState<Todo['name']>('');
 
   const deleteTodo = (todoId: Todo['id']) => {
     setTodos(prevState => {
@@ -15,9 +17,25 @@ function App() {
     });
   }
 
+  useEffect(() =>  {
+    console.log(todos);
+  }, [todos]);
+
+  const handleCreate = () => {
+    const todo: Todo = {
+      id: uuidv4(),
+      name: 'test',
+    }
+    setTodos((prevState) => {
+      return [...prevState, todo];
+    });
+  }
+
+  
   return (
     <div className="App">
-      <Input />
+      <Input value={todoName} placeholder='todo name...'/>
+      <button onClick={handleCreate}>create</button>
       <TodoList deleteTodo={deleteTodo} searchValue={searchValue} setSelectedTodo={setSelectedTodo} todos={todos}/>
     </div>
   );
